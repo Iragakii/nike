10005d75f5384db196d3427427e971a56f126844
unknown <jcgroup.vni@gmail.com>
2024-06-14 13:36:45 +0700

    nike

---
+++ product/product.js
@@ -0,0 +1,8 @@
+const bigImg = document.querySelector(".product-img-big img");
+const smallImgs = document.querySelectorAll(".product-img-small img");
+
+smallImgs.forEach(function(imgItem, index) {
+    imgItem.addEventListener("click", function() {
+        bigImg.src = imgItem.src;
+    });
+});
